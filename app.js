1b958d2e0f3229f208da7059cc350c6c238fcf04
drovocek <mycodepg@gmail.com>
2023-04-29 17:33:31 +0300

    config socket

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 //socket.io setup
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 const port = 3000
 
@@ -15,9 +15,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+const players = {}
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+  console.log('a user connected')
+
+  players[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random(),
+    color: 'yellow'
+  }
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete players[socket.id]
+    io.emit('updatePlayers', players)
+  })
+
+  console.log(players)
+
+  io.emit('updatePlayers', players)
+})
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-const socket = io();
+const socket = io()
 
 const scoreEl = document.querySelector('#scoreEl')
 
@@ -12,15 +12,35 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
+const players = {}
+
+socket.on('updatePlayers', (backendPlayers) => {
+  console.log(backendPlayers)
+  for (const id in backendPlayers) {
+    const backendPlayer = backendPlayers[id]
+
+    if (!players[id]) {
+      players[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+    }
+  }
+  for (const id in players) {
+    if (!backendPlayers[id]) {
+      delete players[id]
+    }
+  }
+})
 
 let animationId
-let score = 0
 function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  player.draw()
+  for (const id in players) {
+    const player = players[id]
+    player.draw()
+  }
+  //players.forEach((player) => player.draw())
 }
 
 animate()
