e7e6d1bf24bc845f4bed09a2c7536f0a9beab9fb
drovocek <mycodepg@gmail.com>
2023-04-29 09:36:54 +0300

    add socket

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+
+//socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +15,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log('server load 222')
